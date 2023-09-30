b805d59b069e91674d0242cf4748dbfc59e27177
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-01 03:16:47 +0600

    Update api.php
    route version concept remvoed

--- routes/api.php
+++ routes/api.php
@@ -9,18 +9,9 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "API" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('skeleton')->group(function () {
 
-Route::prefix('v1')->group(function () {
-    Route::prefix('skeleton')->group(function () {
-
-    });
-});
-
-Route::prefix('v2')->group(function () {
-    Route::prefix('skeleton')->group(function () {
-
-    });
 });
