daa8f1d362cdb4d0d8254d45a72b2723147d62de
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-13 11:38:52 +0600

    Update skeleton.php
    repos auto link sort code added

--- config/skeleton.php
+++ config/skeleton.php
@@ -8,11 +8,26 @@ return [
     | Skeleton Group Root Prefix
     |--------------------------------------------------------------------------
     |
-    | This value will be added to your all routes from this package
-    | Example: APP_URL/{root_prefix}/api/{version}/skeleton/action
+    | This value will be added to all your routes from this package
+    | Example: APP_URL/{root_prefix}/api/skeleton/action
     |
     | Note: while adding prefix add closing ending slash '/'
     */
+    
     'root_prefix' => 'test/',
+
+    //** Model Config Point Do not Remove **//
+
+        /*
+    |--------------------------------------------------------------------------
+    | Repositories
+    |--------------------------------------------------------------------------
+    |
+    | This value will be used across systems where a repositoy instance is needed
+    */
+    
+    'repositories' => [
+        //** Repository Binding Config Point Do not Remove **//
+    ],
 
 ];
